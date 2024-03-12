c5fe9ff4f874d8251e1b4a1aa7b0465df72695d8
DungNB <nbdung0000@gmail.com>
2024-03-13 01:26:36 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/MyStock_Lab3Context.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public partial class MyStock_Lab3Context : DbContext
+    {
+        public MyStock_Lab3Context()
+        {
+        }
+
+        public MyStock_Lab3Context(DbContextOptions<MyStock_Lab3Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DUNGNB\\DUNG_NB;database=MyStock;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.CarId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CarID");
+
+                entity.Property(e => e.CarName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
